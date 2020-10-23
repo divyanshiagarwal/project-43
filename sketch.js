115c27f84fff99d6133bfe9702f9fe9fa5633c61
divyanshiagarwal <69861304+divyanshiagarwal@users.noreply.github.com>
2020-10-23 21:05:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+//Game Design Document
+//Fill up the Following document 
+
+//________________________________________
+
+
+
+//1.	Write the title of your project.
+//Running game
+//________________________________________
+
+
+///2.	What is the goal of the game? 
+//To collect coins 
+//________________________________________
+
+
+//3.	Write a brief story of your game?
+//Divy is running and collecting coins and the monster try to eat divy
+//________________________________________
+
+//________________________________________
+
+//________________________________________
+
+
+ 
+//4.	Which are the playing characters of this game? 
+//●	Playing characters are the ones which respond to the user based on the input from the user
+//●	Cars, monkeys, dinos, wizards etc, are the playing characters in the game.  
+//Number	Character Name	What can this character do? 
+//1	divy	Collect coins
+		
+ 
+//6.	Which are the Non Playing Characters of this game?
+//●	Non Playing characters are the ones which don't have an action or a behaviour when the user interacts with the game.
+//●	Hurdles, stones, bananas, coins, etc are non playing characters in the game.   
+//Number	Character Name	What can this character do? 
+//1	monsters	Eat divy
+
+
+
+
+//Draw your imagination of this game. What does this game look like?
+//●	Draw the game either on your computer or on paper, 
+//●	Add images of scenes of the game to show each of the playing and non-playing characters at least once.  
+
+//Like a subway suffer game
+//________________________________________
+//________________________________________
+//________________________________________
+///________________________________________
+
+//How do you plan to make your game engaging? 
+//________________________________________________________________________________________________________________________________________________________________
+
+
